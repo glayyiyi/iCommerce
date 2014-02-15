a3684caa18368737fbd6e3b9c0809b11ee1c0840
glayyiyi <glay.guo@gmail.com>
2014-02-16 01:16:00 +0800

    update

---
+++ iCommerce/Classes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iCommerce
+//
+//  Created by Glay Guo on 14-2-12.
+//  Copyright (c) 2014年 Glay Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "UMSAgent.h"
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
